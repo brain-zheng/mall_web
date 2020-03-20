fdd11e24403840e4339f9a68732325dc1af78f74
郑海健 <brainhai0619@gmail.com>
2020-03-20 21:22:16 +0800

    first commit

---
+++ src/main/java/com/mall/web/controller/account/LoginController.java
@@ -0,0 +1,23 @@
+package com.mall.web.controller.account;
+
+import com.mall.common.service.domain.ServiceResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zheng haijain
+ * @createTime 2020-03-18 14:23
+ * @description
+ */
+@RestController
+@RequestMapping(value = "/account")
+public class LoginController {
+
+
+
+
+
+
+
+
+}
