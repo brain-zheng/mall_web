60fe333b0bc638e9cd574911a9c7ef5324ba97e6
郑海健 <brainhai0619@gmail.com>
2020-04-13 19:32:48 +0800

    mall商城首页配置模块

--- src/main/java/com/mall/web/common/Constants.java
+++ src/main/java/com/mall/web/common/Constants.java
@@ -8,6 +8,10 @@ public class Constants {
     public static final int RESULT_CODE_PARAM_ERROR = 406;  // 传参错误
     public static final int RESULT_CODE_SERVER_ERROR = 500;  // 服务器错误
 
+    public final static int INDEX_GOODS_HOT_NUMBER = 4;//首页热卖商品数量
+    public final static int INDEX_GOODS_NEW_NUMBER = 5;//首页新品数量
+    public final static int INDEX_GOODS_RECOMMOND_NUMBER = 10;//首页推荐商品数量
+
     public final static int INDEX_CAROUSEL_NUMBER = 5;//首页轮播图数量(可根据自身需求修改)
 
     public final static int INDEX_CATEGORY_NUMBER = 10;//首页一级分类的最大数量
--- src/main/java/com/mall/web/controller/IndexController.java
+++ src/main/java/com/mall/web/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.mall.web.controller;
 
+import com.mall.admincenter.client.dto.MallIndexConfigGoodsDTO;
+import com.mall.admincenter.client.service.IndexConfigService;
 import com.mall.web.common.Constants;
+import com.mall.web.enums.IndexConfigTypeEnum;
 import com.mall.web.manager.IndexManager;
 import com.mall.web.response.MallIndexCarouselVO;
 import com.mall.web.response.MallIndexCategoryVO;
@@ -23,6 +26,9 @@ public class IndexController {
     @Resource
     private IndexManager indexManager;
 
+    @Resource
+    private IndexConfigService indexConfigService;
+
     @RequestMapping(value = {"/", "/index","/index.html"}, method = RequestMethod.GET)
     public String indexPage(HttpServletRequest request){
         List<MallIndexCategoryVO> categories = indexManager.getCategoriesForIndex();
@@ -30,12 +36,15 @@ public class IndexController {
             return "error/error_5xx";
         }
         List<MallIndexCarouselVO> carousels = indexManager.getCarouselsForIndex(Constants.INDEX_CAROUSEL_NUMBER);
+        List<MallIndexConfigGoodsDTO> hotGoodses = indexConfigService.getConfigGoodsesForIndex(IndexConfigTypeEnum.INDEX_GOODS_HOT.getType(), Constants.INDEX_GOODS_HOT_NUMBER);
+        List<MallIndexConfigGoodsDTO> newGoodses = indexConfigService.getConfigGoodsesForIndex(IndexConfigTypeEnum.INDEX_GOODS_NEW.getType(), Constants.INDEX_GOODS_NEW_NUMBER);
+        List<MallIndexConfigGoodsDTO> recommendGoodses = indexConfigService.getConfigGoodsesForIndex(IndexConfigTypeEnum.INDEX_GOODS_RECOMMOND.getType(), Constants.INDEX_GOODS_RECOMMOND_NUMBER);
 
         request.setAttribute("categories", categories);//分类数据
         request.setAttribute("carousels", carousels);//轮播图
-//        request.setAttribute("hotGoodses", hotGoodses);//热销商品
-//        request.setAttribute("newGoodses", newGoodses);//新品
-//        request.setAttribute("recommendGoodses", recommendGoodses);//推荐商品
+        request.setAttribute("hotGoodses", hotGoodses);//热销商品
+        request.setAttribute("newGoodses", newGoodses);//新品
+        request.setAttribute("recommendGoodses", recommendGoodses);//推荐商品
 
         return "mall/index";
     }
